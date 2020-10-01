c7822ce5e17fd4b8c06b432406a3dc0e4a3ad571
Elliot Kyei <ekyei1@myseneca.ca>
2020-10-01 19:09:33 -0400

    route change

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ function onHttpStart() {
 }
 
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/home", function(req,res){
+app.get("/", function(req,res){
     res.send("Hello World<br /><a href='/Home'>Go to the home page</a>");
 });
 
 // setup another route to listen on /about
-app.get("/home", function(req,res){
+app.get("/Home", function(req,res){
     res.sendFile(path.join(__dirname,"/views/Home.html"));
   });
 
