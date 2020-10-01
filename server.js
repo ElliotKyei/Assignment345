eead2098642e1cecc7ebaff9dc6aa96023e094c7
Elliot Kyei <ekyei1@myseneca.ca>
2020-10-01 18:57:22 -0400

    spelling error on Home

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ function onHttpStart() {
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
-    res.send("Hello World<br /><a href='/home'>Go to the home page</a>");
+    res.send("Hello World<br /><a href='/Home'>Go to the home page</a>");
 });
 
 // setup another route to listen on /about
 app.get("/home", function(req,res){
-    res.sendFile(path.join(__dirname,"/views/home.html"));
+    res.sendFile(path.join(__dirname,"/views/Home.html"));
   });
 
 // setup http server to listen on HTTP_PORT
