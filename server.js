601dde2e0ce558d7bdc1fb029ee27dbb1562ee04
Elliot Kyei <ekyei1@myseneca.ca>
2020-10-02 18:17:06 -0400

    route change

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get("/", function(req,res){
 });
 
 app.get("/home", function(req,res){
-    res.sendFile(path.join(__dirname,"/home.html"));
+    res.sendFile(path.join(__dirname,"views/home.html"));
   });
 
 
