c288d38d1b3586a768e6b6ac76a75ba1fde7f119
Elliot Kyei <ekyei1@myseneca.ca>
2020-10-02 18:10:57 -0400

    complete version 1

--- server.js
+++ server.js
@@ -2,23 +2,23 @@ var path = require("path");
 var express = require("express");
 var app = express();
 
-var HTTP_PORT = process.env.PORT || 8080;
+var HTTP_PORT = process.env.PORT || 8081;
 
-// call this function after the http server starts listening for requests
+
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-// setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
-    res.send("Hello World<br /><a href='/home'>Go to the home page</a>");
+    res.send("Hello World<br /><a href='/home.html'>Go to the home page</a>");
 });
 
-// setup another route to listen on /about
 app.get("/home", function(req,res){
     res.sendFile(path.join(__dirname,"/views/home.html"));
   });
 
-  app.use(express.static('public'))
-// setup http server to listen on HTTP_PORT
+
+  app.use('/static', express.static('static'))
+  app.use(express.static('./views'))
+
 app.listen(HTTP_PORT, onHttpStart);
---
+++ views/data-service.js
@@ -0,0 +1,43 @@
+//Top Meal Packages
+
+let TopMealPackage = [
+{
+    name: "Chicken Breast with Tomatoes", 
+    price: 10.99
+},
+{
+    name: "Pea and Spinach Carbonara", 
+    price: 10.99
+},
+{
+    name: "Potato Salad with Mackeral", 
+    price: 11.99
+},
+{
+    name: "Beef and Vegetable Stir Fry", 
+    price: 12.99
+}];
+
+// Meal Package Listings
+
+let MealPackageListing =  [
+{
+name: "Muscle Gain", 
+description: "4 meals of lean cuts of meats, fish, eggs and poultry, which are very high in protein and calories, low in carbs",
+price: 25.99
+},
+{
+name: "Weight Loss", 
+description: "7 small meals of very lean and nutritious cuts of meat, whole wheat grains, fruits and vegetables. Each meal is high in protein and low in carbs & calories",
+price: 27.99
+},
+{
+name: "Keto", 
+description: "4 meals featuring nuts, lean meats and fish, fruits and vegetables. Each meal is high in healthy fats with adequate protein, and minimal in carbs & calories",
+price: 25.99
+},
+{
+name: "Paleo", 
+description: "4 meals of lean meats and fish, whole wheat grains, fruits and vegetables. Each meal is moderate in protein, carbs, and healthy fats. 100% All Natural Produce",
+price: 25.99
+}];
