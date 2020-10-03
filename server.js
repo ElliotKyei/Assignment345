564987e05b54dbf092d8106c628b82e6c0070706
Elliot Kyei <ekyei1@myseneca.ca>
2020-10-02 20:57:17 -0400

    good copy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var path = require("path");
 var express = require("express");
 var app = express();
 
-var HTTP_PORT = process.env.PORT || 8081;
+var HTTP_PORT = process.env.PORT || 8082;
 
 
 function onHttpStart() {
